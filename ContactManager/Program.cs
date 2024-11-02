9fbf01cb7bd8203fb56a3b6f2e416bd15caff65e
murilonicemento <murilo_nascimento22@outlook.com>
2024-11-02 19:54:40 -0300

    change singleton services to scoped services

--- ContactManager/Program.cs
+++ ContactManager/Program.cs
@@ -10,8 +10,8 @@ using Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddSingleton<ICountriesService, CountriesService>();
-builder.Services.AddSingleton<IPersonsService, PersonsService>();
+builder.Services.AddScoped<ICountriesService, CountriesService>();
+builder.Services.AddScoped<IPersonsService, PersonsService>();
 builder.Services.AddDbContext<PersonsDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty);
